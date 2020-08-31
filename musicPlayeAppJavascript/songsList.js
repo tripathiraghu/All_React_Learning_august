9de449b43bf61d00db7bf677980939e97f5c4eb7
TripathiRaghu <tripathiraghu1@gmail.com>
2020-08-31 20:27:05 +0530

    re

---
+++ musicPlayeAppJavascript/songsList.js
@@ -0,0 +1,71 @@
+const songs = [
+		{
+			title : "Bekhyali",
+			artist: "NO name",
+			audio: "music/music-1.mp3",
+			image: "images/music-1.jpg"
+
+		},
+		{
+			title : "Ghoomer",
+			artist: "Not Provided",
+			audio: "music/music-2.mp3",
+			image: "images/music-2.jpg"
+
+		},
+		{
+			title : "Bom Digi",
+			artist: "Unknown",
+			audio: "music/music-3.mp3",
+			image: "images/music-3.jpg"
+
+		},
+		{
+			title : "Khairiyat",
+			artist: "Unknown",
+			audio: "https://cdn10.upload.solutions/4f555ab7891f1e44902ba369388f7330/agjov/Khairiyat%20%20Sad%20-(Mr-Jatt.com).mp3",
+			image: "https://static.upload.solutions/thumbs/100_100/vtpfi.jpg"
+
+		},
+		{
+			title : "8 Parche",
+			artist: "Gur Sidhu , Baani Sandhu",
+			audio: "https://cdn10.upload.solutions/bf1225404580b9212d03d71130cdef9a/uojov/8%20Parche-(Mr-Jatt.com).mp3",
+			image: "https://static.upload.solutions/thumbs/100_100/iwpfi.jpg"
+
+		},
+		{
+			title : "Jatti Da Crush",
+			artist: " Siamil Shah , Kay Vee Singh",
+			audio: "https://cdn10.upload.solutions/3e1d1d6b4c8de68c4600f1ad9811c247/wmgov/Jatti%20Da%20Crush-(Mr-Jatt.com).mp3",
+			image: "https://static.upload.solutions/thumbs/100_100/wmpfi.jpg"
+
+		},
+		{
+			title : "Jee Karr Daa",
+			artist: "Harrdy Sandhu",
+			audio: "https://jatt.download/dren/music/data/Single_Track/202003/Jee_Karr_Daa/128/Jee_Karr_Daa_1.mp3/Jee%20Karr%20Daa.mp3",
+			image: "https://cover.mr-jatt.im/thumb/488025/Jee-Karr-Daa.jpg"
+
+		},
+		{
+			title : "Kya Baat Ay",
+			artist: "Harrdy Sandhu",
+			audio: "https://jatt.download/dren/music/data/Single_Track/201809/Kya_Baat_Ay/128/Kya_Baat_Ay_1.mp3/Kya%20Baat%20Ay.mp3",
+			image: "https://cover.mr-jatt.im/thumb/480966/Kya-Baat-Ay.jpg"
+
+		},{
+			title : "Hawa Banke",
+			artist: " Darshan Raval",
+			audio: "https://jatt.download/dren/music/data/New_Talent/201907/Hawa_Banke_Darshan_Raval_Official_Song/128/Hawa_Banke_Darshan_Raval_Official_Song.mp3/Hawa%20Banke%20Darshan%20Raval%20Official%20Song.mp3",
+			image: "https://cover.mr-jatt.im/thumb/485629/Hawa-Banke.jpg"
+
+		},{
+			title : "Tera Chehra (Tera Chehra)",
+			artist: " Adnan Sami",
+			audio: "https://cdn5.upload.solutions/927227d2700c618f8bd20d5377d6f2e4/dligv/Tera%20Chehra%20%20Tera%20Chehra%20-(Mr-Jatt.com).mp3",
+			image: ""
+
+		}
+
+	];
